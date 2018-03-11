bc2dfd2392b7edad55d4dfd682fa4e2fcddf7214
zulonghao <726461151@qq.com>
2018-03-11 19:48:28 +0800

    添加输出5555

--- src/main/java/Test3.java
+++ src/main/java/Test3.java
@@ -5,5 +5,8 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("dfsd");
+        System.out.println("sdfjiosjdf");
+        System.out.println("yyyyy");
+        System.out.println("erdty4");
     }
 }
